0f06f24738b4df8b31a93a9d2a2fb707d80b5f6a
liuyang.0826 <liuyang.0826@bytedance.com>
2021-12-30 03:14:22 +0800

    feat: schema预编译逻辑优化

--- packages/workspace/build/compile-schemas.js
+++ packages/workspace/build/compile-schemas.js
@@ -9,35 +9,14 @@ function compileSchemas(code) {
     sourceType: "module"
   })
   const s = new MagicString(code)
+  const returnList = []
   let prevStart = 0
   let end = 0
-  let returnList = []
   let replaceNode
+  let replacePath
   let exportNode
   traverse.default(ast, {
     enter(path) {
-      if (replaceNode && t.isIdentifier(path.node)) {
-        if (
-          (path.scope.block === exportNode || t.isProgram(path.scope.block)) // 通过作用域锁定范围
-          && (
-            (t.isObjectProperty(path.parent) && path.parent.value === path.node) // 对象值
-            || t.isArrayExpression(path.parent) // 数组元素
-            || t.isReturnStatement(path.parent) // 直接return
-            || t.isSpreadElement(path.parent) // ...
-            || t.isCallExpression(path.parent) // 函数调用
-          )
-        ) {
-          const { start, end, name } = path.node
-          if (start > prevStart) {
-            returnList.push({ type: "string", content: s.slice(prevStart, start)})
-            if (path.parent.shorthand) {
-              returnList.push({ type: "string", content: `${name}: `})
-            }
-            returnList.push({ type: "var", content: s.slice(start, end) })
-            prevStart = end
-          }
-        }
-      }
       if (t.isExportDefaultDeclaration(path.node)) {
         exportNode = path.node.declaration
       }
@@ -58,23 +37,41 @@ function compileSchemas(code) {
           end = path.node.end
         }
       }
+      if (!replacePath && path.node === replaceNode) {
+        replacePath = path
+      }
+      if (
+        replaceNode && t.isIdentifier(path.node)
+        && (path.scope.block === exportNode || t.isProgram(path.scope.block)) // 通过作用域锁定范围
+        && (
+          (t.isObjectProperty(path.parent) && path.parent.value === path.node) // 对象值
+          || t.isArrayExpression(path.parent) // 数组元素
+          || t.isReturnStatement(path.parent) // 直接return
+          || t.isSpreadElement(path.parent) // ...
+          || t.isCallExpression(path.parent) // 函数调用
+        )
+      ) {
+        const { start, end, name } = path.node
+        if (start > prevStart) {
+          returnList.push({ type: "string", content: s.slice(prevStart, start)})
+          if (path.parent.shorthand) {
+            returnList.push({ type: "string", content: `${name}: `})
+          }
+          returnList.push({ type: "var", content: s.slice(start, end) })
+          prevStart = end
+        }
+      }
     }
   });
   returnList.push({
     type: "string",
     content: s.slice(prevStart, end)
   })
-  traverse.default(ast, {
-    enter(path) {
-      if (path.node === replaceNode) {
-        path.replaceWith(
-          returnList.reduce((acc, cur) => {
-            return (binaryExpression) => acc(t.binaryExpression("+", cur.type === "string" ? t.stringLiteral(cur.content) : t.identifier(cur.content), binaryExpression))
-          }, (binaryExpression) => t.binaryExpression("+", t.stringLiteral(""), binaryExpression))(t.stringLiteral(""))
-        )
-      }
-    }
-  })
+  replacePath.replaceWith(
+    returnList.reduce((acc, cur) => {
+      return (binaryExpression) => acc(t.binaryExpression("+", cur.type === "string" ? t.stringLiteral(cur.content) : t.identifier(cur.content), binaryExpression))
+    }, (binaryExpression) => t.binaryExpression("+", t.stringLiteral(""), binaryExpression))(t.stringLiteral(""))
+  )
   return generate.default(ast, {}).code
 }
 
