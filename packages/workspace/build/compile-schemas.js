5dca9d9c00bc2fa802e95a49e67e38cc578c1a99
liuyang.0826 <liuyang.0826@bytedance.com>
2021-12-30 14:00:09 +0800

    feat: babel解析逻辑完善

--- packages/workspace/build/compile-schemas.js
+++ packages/workspace/build/compile-schemas.js
@@ -2,16 +2,26 @@ const babelParser = require("@babel/parser")
 const t = require("@babel/types")
 const traverse = require("@babel/traverse");
 const generate = require("@babel/generator")
-const MagicString = require('magic-string')
+
+function findRefScope(path) {
+  const name = path.node.name
+  let curScope = path.scope
+  while (curScope) {
+    if (curScope.hasOwnBinding(name)) {
+      return curScope
+    }
+    curScope = curScope.parent
+  }
+  return null
+}
+
 
 function compileSchemas(code) {
   const ast = babelParser.parse(code, {
     sourceType: "module"
   })
-  const s = new MagicString(code)
   const returnList = []
   let prevStart = 0
-  let end = 0
   let replaceNode
   let replacePath
   let exportNode
@@ -22,55 +32,61 @@ function compileSchemas(code) {
       }
       if (exportNode && !replaceNode) {
         if (t.isExportDefaultDeclaration(path.parent)) {
+          replaceNode = path.node
           if (t.isArrowFunctionExpression(path.node)) {
-            replaceNode = path.node.body
-            prevStart = path.node.body.start
-            end = path.node.body.end
-          } else {
-            replaceNode = path.node
-            prevStart = path.node.start
-            end = path.node.end
+            replaceNode = replaceNode.body
           }
-        } else if (t.isReturnStatement(path.node) && (path.scope.block === exportNode || t.isProgram(path.scope.block))) {
-          replaceNode = path.node.argument
-          prevStart = path.node.start + "return ".length
-          end = path.node.end
+        } else if (t.isReturnStatement(path.node)) {
+          const refScope = findRefScope(path)
+          if ((!refScope || refScope.block === exportNode || t.isProgram(refScope.path.node))) {
+            replaceNode = path.node.argument
+          }
         }
       }
       if (!replacePath && path.node === replaceNode) {
         replacePath = path
+        prevStart = replaceNode.start
       }
-      if (
-        replaceNode && t.isIdentifier(path.node)
-        && (path.scope.block === exportNode || t.isProgram(path.scope.block)) // 通过作用域锁定范围
-        && (
-          (t.isObjectProperty(path.parent) && path.parent.value === path.node) // 对象值
-          || t.isArrayExpression(path.parent) // 数组元素
-          || t.isReturnStatement(path.parent) // 直接return
-          || t.isSpreadElement(path.parent) // ...
-          || t.isCallExpression(path.parent) // 函数调用
-        )
-      ) {
-        const { start, end, name } = path.node
-        if (start > prevStart) {
-          returnList.push({ type: "string", content: s.slice(prevStart, start)})
-          if (path.parent.shorthand) {
-            returnList.push({ type: "string", content: `${name}: `})
+      if (replacePath && t.isIdentifier(path.node)) {
+        const refScope = findRefScope(path)
+        if (
+          (!refScope || refScope.block === exportNode || t.isProgram(refScope.path.node))
+          && (
+            (t.isObjectProperty(path.parent) && path.parent.value === path.node) // 对象值
+            || t.isArrayExpression(path.parent) // 数组元素
+            || t.isReturnStatement(path.parent) // 直接return
+            || t.isSpreadElement(path.parent) // ... 结构运算符
+            || t.isCallExpression(path.parent) // 函数调用
+            || t.isBinaryExpression(path.parent) // 字符串表达式
+            || t.isTemplateLiteral(path.parent) // 模板字符串
+          )
+        ) {
+          const { start, end, name } = path.node
+          if (start > prevStart) {
+            returnList.push(t.stringLiteral(code.slice(prevStart, start)))
+            if (path.parent.shorthand) {
+              returnList.push(t.stringLiteral(`${name}: `))
+            }
+            // 函数调用保留原表达式
+            if (t.isCallExpression(path.parent)) {
+              returnList.push(path.parent)
+              prevStart = path.parent.end
+            } else {
+              returnList.push(t.identifier(code.slice(start, end)))
+              prevStart = end
+            }
           }
-          returnList.push({ type: "var", content: s.slice(start, end) })
-          prevStart = end
         }
       }
     }
   });
-  returnList.push({
-    type: "string",
-    content: s.slice(prevStart, end)
-  })
+  returnList.push(t.stringLiteral(code.slice(prevStart, replaceNode.end)))
   replacePath.replaceWith(
-    returnList.reduce((acc, cur) => {
-      return (binaryExpression) => acc(t.binaryExpression("+", cur.type === "string" ? t.stringLiteral(cur.content) : t.identifier(cur.content), binaryExpression))
-    }, (binaryExpression) => t.binaryExpression("+", t.stringLiteral(""), binaryExpression))(t.stringLiteral(""))
+    returnList.reduce(
+      (acc, cur) => (binaryExpression) => acc(
+        t.binaryExpression("+", cur, binaryExpression)
+      ), binaryExpression => t.binaryExpression("+", t.stringLiteral(""), binaryExpression)
+    )(t.stringLiteral(""))
   )
   return generate.default(ast, {}).code
 }
